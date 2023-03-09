de20d8bad8bf5a7bc437a7ff7efb59853209f232
Matthew Kaye <matthewkaye@Matthews-MBP.fios-router.home>
2023-03-09 10:36:58 -0500

    Added card template to HTML code. Weather info is now pulled with the test example.

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,10 @@
-var apiKey = '';
+var apiKey = '60c3eea7f8396024e6ea304c296ccf7a'
 var city = document.getElementById('cityInput');
 var searchButton = document.getElementById('search-button');
+var latSearch;
+var lonSearch;
 var lat = '33.71745'
 var lon = '18.9963167'
-var geoSearchURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
 var geoSearchTest = 'https://api.openweathermap.org/geo/1.0/direct?q=chicago&limit=5&appid=60c3eea7f8396024e6ea304c296ccf7a'
 var weatherSearchTest = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.71745&lon=18.9963167&appid=60c3eea7f8396024e6ea304c296ccf7a'
 var weatherSearchURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
@@ -12,28 +13,35 @@ var weatherSearchURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '
 
 function getLatLon() {
     var citySearch = city.value.trim();
+    var geoSearchURL = 'https://api.openweathermap.org/geo/1.0/direct?q='+citySearch+'&limit=5&appid='+apiKey;
     console.log(citySearch);
-    fetch(geoSearchTest)
+    fetch(geoSearchURL)
     .then(function(response) {
-            console.log(response)
-            response.json().then(function(data) {
-                console.log(data)
-                // Add in pushing coordinates into local storage
-            })
+        console.log(response)
+        response.json().then(function(data) {
+        console.log(data);
+        console.log(data.length);
+        console.log(data[0].state);
+        console.log(data[0].country);
+        latSearch = data[0].lat;
+        lonSearch = data[0].lon;
+        weatherPost()
+        })
     })
 };
 
-// function weatherPost() {
-//     // var lat = localStorage.getItem("citylat")
-//     // var lon = localStorage.getItem("citylon")
+function weatherPost() {
+    
 
-//     fetch(weatherSearchTest)
-//     .then(function(response) {
-//             console.log(response)
-//             response.json().then(function(data) {
-//                 console.log(data)
-//             })
-//     })
-// }
+    fetch(weatherSearchTest)
+    .then(function(response) {
+            console.log(response)
+            response.json().then(
+                function(data) {
+                console.log(data)
+                
+            })
+    })
+}
 
 searchButton.addEventListener("click", getLatLon);
