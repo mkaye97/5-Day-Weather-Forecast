f01b701c5052b1d2bd79d50a413bd898b05a53da
mkaye <mattkaye97@gmail.com>
2023-03-30 01:54:35 -0400

    Weather API responses now working. Added base card to html file.

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,53 @@
-var apiKey = '60c3eea7f8396024e6ea304c296ccf7a';
+var apiKey = '1907264457499489c3984f23a366b7ad';
 var city = document.getElementById('cityInput');
 var searchButton = document.getElementById('search-button');
-var lat = '33.71745'
-var lon = '18.9963167'
-var geoSearchURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
-var geoSearchTest = 'https://api.openweathermap.org/geo/1.0/direct?q=chicago&limit=5&appid=60c3eea7f8396024e6ea304c296ccf7a'
-var weatherSearchTest = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.71745&lon=18.9963167&appid=60c3eea7f8396024e6ea304c296ccf7a'
-var weatherSearchURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+var searchList = document.getElementById('codes');
+var lat;
+var lon;
+var searchHistory = [];
+var weatherData = [];
+// var icon;
+// var iconURL = 'http://openweathermap.org/img/w/' + icon + '.png';
 
 
 
-function getLatLon() {
+
+function getCities() {
     var citySearch = city.value.trim();
+    var geoSearchURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + citySearch + '&limit=1&appid=' + apiKey;
     console.log(citySearch);
-    fetch(geoSearchTest)
-    .then(function(response) {
+    fetch(geoSearchURL)
+        .then(function (response) {
             console.log(response)
-            response.json().then(function(data) {
-                console.log(data)
-                // Add in pushing coordinates into local storage
+            response.json().then(function (data) {
+                console.log(data);
+                lat = data[0].lat.toString();
+                lon = data[0].lon.toString();
+                searchHistory.splice(0, 0, data[0].name);
+                console.log(searchHistory);
+                new newWeatherPost(lat, lon);
+            }
+            )
+        })
+}
+
+function newWeatherPost(coor1, coor2) {
+    var weatherSearchURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + coor1 + '&lon=' + coor2 + '&units=imperial&appid=' + apiKey;
+    fetch(weatherSearchURL)
+        .then(function (response) {
+            response.json().then(function (data) {
+                console.log(data.list);
+                // Store Weather Data
+                // weatherData = []
+                // for (i = 0; data.list.length; i++) {
+                //     var arry = data.list[i].split(' ')
+                //     if (arry[1] == '12:00:00') {
+                //         weatherData.push(data.list[i])
+                //         console.log(weatherData);
+                //     }
+                // }
             })
-    })
-};
+        })
+}
 
-// function weatherPost() {
-//     // var lat = localStorage.getItem("citylat")
-//     // var lon = localStorage.getItem("citylon")
-
-//     fetch(weatherSearchTest)
-//     .then(function(response) {
-//             console.log(response)
-//             response.json().then(function(data) {
-//                 console.log(data)
-//             })
-//     })
-// }
-
-searchButton.addEventListener("click", getLatLon);
+searchButton.addEventListener("click", getCities);
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ SO THAT I can plan a trip accordingly
 
 ## Acceptance Criteria
 
-```
+```md
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -18,4 +18,9 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
-```# 5-Day-Weather-Forecast
+```
+
+### Further Help
+
+- weather data should append the page
+- set search history to local storage and append page
